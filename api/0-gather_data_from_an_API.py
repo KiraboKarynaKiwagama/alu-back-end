9c803ba5ddce7053d4d4d2ddd216d333bab1c1dd
KiraboKarynaKiwagama <k.kiwagama@alustudent.com>
2026-07-01 11:32:47 -0700

    start

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,42 @@
+import requests
+import sys
+ 
+ 
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: {} employee_id".format(sys.argv[0]))
+        sys.exit(1)
+ 
+    try:
+        employee_id = int(sys.argv[1])
+    except ValueError:
+        print("employee_id must be an integer")
+        sys.exit(1)
+ 
+    base_url = "https://jsonplaceholder.typicode.com"
+ 
+    user_response = requests.get("{}/users/{}".format(base_url, employee_id))
+    if user_response.status_code != 200:
+        print("Employee not found")
+        sys.exit(1)
+ 
+    user_data = user_response.json()
+    employee_name = user_data.get("name")
+ 
+    todos_response = requests.get(
+        "{}/todos".format(base_url),
+        params={"userId": employee_id}
+    )
+    todos_data = todos_response.json()
+ 
+    total_tasks = len(todos_data)
+    done_tasks = [task for task in todos_data if task.get("completed") is True]
+    number_of_done_tasks = len(done_tasks)
+ 
+    print("Employee {} is done with tasks({}/{}):".format(
+        employee_name, number_of_done_tasks, total_tasks))
+ 
+    for task in done_tasks:
+        print("\t {}".format(task.get("title")))
+ 
+
